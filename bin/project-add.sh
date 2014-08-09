60aef3b1ecd209aba8ff757500055f0f3fe7dedb
onexhovia <onexhovia@gmail.com>
2014-08-09 23:25:03 +0600

    Update project utils

--- bin/project-add.sh
+++ bin/project-add.sh
@@ -8,3 +8,28 @@ if [ $EUID -ne 0 ]; then
   echo "This script should be run as root." > /dev/stderr
   exit 1
 fi
+
+PROJECT_USER="dev"
+PROJECT_PATH="/home/$PROJECT_USER"
+PROJECT_NAME="$1"
+PROJECT_PATH_FULL=$(echo "$PROJECT_PATH/$PROJECT_NAME")
+
+URL_BITRIXSETUP="http://www.1c-bitrix.ru/download/scripts/bitrixsetup.php"
+URL_BITRIXRESTOR="http://www.1c-bitrix.ru/download/scripts/restore.php"
+
+if [ "$PROJECT_NAME" == '' ]; then
+  read -p "Enter name project and press [ENTER]: " PROJECT_NAME
+fi
+
+if [ -d "$PROJECT_PATH_FULL" ]; then
+  echo "Project '$PROJECT_NAME' already exists."
+  exit 1
+fi
+
+mkdir -p ${PROJECT_PATH_FULL}
+
+wget ${URL_BITRIXSETUP} -P ${PROJECT_PATH_FULL}
+wget ${URL_BITRIXRESTOR} -P ${PROJECT_PATH_FULL}
+
+chown -R "$PROJECT_USER:$PROJECT_USER" ${PROJECT_PATH_FULL}
+chmod -R g+rwx ${PROJECT_PATH_FULL}
--- bin/project-remove.sh
+++ bin/project-remove.sh
@@ -8,3 +8,15 @@ if [ $EUID -ne 0 ]; then
   echo "This script should be run as root." > /dev/stderr
   exit 1
 fi
+
+PROJECT_USER="dev"
+PROJECT_PATH="/home/$PROJECT_USER"
+PROJECT_NAME="$1"
+
+if [ "$PROJECT_NAME" == '' ]; then
+  read -p "Enter name project and press [ENTER]: " PROJECT_NAME
+fi
+
+if [ -d "$PROJECT_PATH/$PROJECT_NAME" ]; then
+  rm -rf "$PROJECT_PATH/$PROJECT_NAME"
+fi
